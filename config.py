1d3698331a78dcf2920540477e2b8fc72e2300e0
mayureshkumar56 <mayureshk741@gmail.com>
2026-05-27 16:45:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+# ============================================================
+#   CONFIG FILE — Sirf yahan apna BOT_TOKEN paste karo
+# ============================================================
+
+BOT_TOKEN = "8760535655:AAG6GZuzavTT8c5TRIWrsaznqi4F5MlP26g"
+
+# ============================================================
+#   GROUPS CONFIG — Chat IDs aur limits
+# ============================================================
+
+PRIVATE_BACKUP_CHAT_ID = -1003938588965
+
+GROUPS_CONFIG = {
+    "A": {"name": "Group A", "chat_id": -1003810828255, "limit": 600},
+    "B": {"name": "Group B (Strict Adult Filter)", "chat_id": -1003791464998, "limit": 600},
+    "C": {"name": "Group C", "chat_id": -1003956008527, "limit": 600},
+    "D": {"name": "Group D", "chat_id": -1003934725687, "limit": 600},
+    "F": {"name": "Group F (Fallback & Small Preview)", "chat_id": 3959279305, "limit": 99999}
+}
+
+# ============================================================
+#   BLOCKLIST — Ye domains Group B mein jayenge
+# ============================================================
+
+TARGET_BLOCKLIST = [
+    "xhamster.com", "xhamster45.desi", "xhamster2.com", "xhamster.desi",
+    "freepornvideo.sex", "aagmaal.dog", "xhopen.com", "beeg.porn", "desixx.net",
+    "wonporn.com", "xxxvideosind.com", "analvids.com",
+]
